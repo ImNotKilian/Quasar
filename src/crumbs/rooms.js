53d2963410600e48dde60506bd78d9410918a824
Zaseth <hahano>
2019-06-30 19:13:40 +0200

    More rooms

--- src/crumbs/rooms.js
+++ src/crumbs/rooms.js
@@ -6,5 +6,9 @@
  */
 module.exports = {
   100: { name: 'Town', max: 120 },
-  110: { name: 'Coffee Shop', max: 80 }
+  110: { name: 'Coffee Shop', max: 80 },
+  120: { name: 'Night Club', max: 80 },
+  121: { name: 'Arcade', max: 80 },
+  122: { name: 'Recycling Plant', max: 80 },
+  130: { name: 'Clothes Shop', max: 80 }
 }
