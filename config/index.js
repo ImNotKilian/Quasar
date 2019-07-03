8fe6abb9423f50980e3a681cd65094aeeb15e836
Zaseth <hahano>
2019-07-03 18:55:07 +0200

    Changed world port to default from mediaserver

--- config/index.js
+++ config/index.js
@@ -11,7 +11,7 @@ module.exports = {
 
   LOGIN: { HOST: '127.0.0.1', PORT: 6112, MAX: 100 },
 
-  WORLD: { HOST: '127.0.0.1', PORT: 6113, MAX: 200 },
+  WORLD: { HOST: '127.0.0.1', PORT: 9875, MAX: 200 },
 
   DATABASE: { host: '127.0.0.1', user: 'root', password: '', database: 'quasar' }
 }
