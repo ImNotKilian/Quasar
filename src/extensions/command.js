e29c5de4cb00cab3b6356e429966802f1ee41950
Zaseth <hahano>
2019-07-12 13:12:17 +0200

    Added some more commands

--- src/extensions/command.js
+++ src/extensions/command.js
@@ -5,7 +5,12 @@
  * @constant
  */
 const commands = {
-  'jr': { 'func': 'joinRoom', 'len': 1, mod: false }
+  'jr': { 'enabled': true, 'func': 'joinRoom', 'len': 1, mod: true },
+  'ai': { 'enabled': true, 'func': 'addItem', 'len': 1, mod: true },
+  'ac': { 'enabled': true, 'func': 'addCoins', 'len': 1, mod: true },
+  'rc': { 'enabled': true, 'func': 'removeCoins', 'len': 1, mod: true },
+  'teleport': { 'enabled': true, 'func': 'teleportTo', 'len': 1, mod: true },
+  'kick': { 'enabled': true, 'func': 'kickPenguin', 'len': 1, mod: true }
 }
 
 /**
@@ -39,7 +44,7 @@ module.exports = class {
         const args = message.substr(1).split(' ')
         const command = args.shift()
 
-        if (commands[command]) {
+        if (commands[command] && commands[command].enabled) {
           const { func, len, mod } = commands[command]
 
           if (args.length === len && penguin.moderator === mod) {
@@ -67,6 +72,80 @@ module.exports = class {
 
       if (room && !penguin.isRoomFull(roomId)) {
         penguin.joinRoom(room)
+      }
+    }
+  }
+
+  /**
+   * Handle the !ai command
+   * @param {String} itemId
+   * @param {Penguin} penguin
+   */
+  static async addItem(itemId, penguin) {
+    itemId = parseInt(itemId)
+
+    if (penguin.inventory.indexOf(itemId) === -1) {
+      penguin.inventory.push(itemId)
+
+      await penguin.server.database.knex('inventory').insert({ id: penguin.id, itemId })
+
+      penguin.sendXt('ai', itemId, penguin.coins)
+    } else {
+      penguin.sendError(400)
+    }
+  }
+
+  /**
+   * Handle the !ac command
+   * @param {String} amount
+   * @param {Penguin} penguin
+   */
+  static async addCoins(amount, penguin) {
+    await penguin.addCoins(parseInt(amount))
+    penguin.sendXt('zo', penguin.coins, '', 0, 0, 0)
+  }
+
+  /**
+   * Handle the !rc command
+   * @param {String} amount
+   * @param {Penguin} penguin
+   */
+  static async removeCoins(amount, penguin) {
+    await penguin.removeCoins(parseInt(amount))
+    penguin.sendXt('zo', penguin.coins, '', 0, 0, 0)
+  }
+
+  /**
+   * Handle the !teleport command
+   * @param {String} id
+   * @param {Penguin} penguin
+   */
+  static teleportTo(id, penguin) {
+    id = parseInt(id)
+
+    if (id !== penguin.id) {
+      const findObj = penguin.server.getPenguinById(id)
+
+      if (findObj && findObj.room && !findObj.isRoomFull(findObj.room.id)) {
+        penguin.removeFromRoom()
+        penguin.joinRoom(findObj.room)
+      }
+    }
+  }
+
+  /**
+   * Handle the !kick command
+   * @param {String} id
+   * @param {Penguin} penguin
+   */
+  static kickPenguin(id, penguin) {
+    id = parseInt(id)
+
+    if (id !== penguin.id) {
+      const kickObj = penguin.server.getPenguinById(id)
+
+      if (kickObj && kickObj.room) {
+        kickObj.sendError(5, true)
       }
     }
   }
